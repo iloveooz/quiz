1372d0e9227df942c94396449501d381cc977023
Sergey Piskunov <iloveooz@yandex.ru>
2019-12-26 10:47:26 +0700

    Create 015-exception.cpp

---
+++ 015-exception.cpp
@@ -0,0 +1,40 @@
+#include <exception>
+#include <iostream>
+
+int x = 0;
+
+class A {
+public:
+	A() {
+		std::cout << 'a';
+		if (x++ == 0) {
+			throw std::exception();
+		}
+	}
+	~A() { std::cout << 'A'; }
+};
+
+class B {
+public:
+	B() { std::cout << 'b'; }
+	~B() { std::cout << 'B'; }
+	A a;
+};
+
+void foo() { static B b; }
+
+int main() {
+	try {
+		foo();
+	}
+	catch (std::exception &) {
+		std::cout << 'c';
+		foo();
+	}
+
+	system("pause");
+	return 0;
+}
+
+// acabBA
+// 
