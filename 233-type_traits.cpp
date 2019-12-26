ac32b5476e1553dc2e49ac13a1473af02cf19c76
Sergey Piskunov <iloveooz@yandex.ru>
2019-12-26 10:35:38 +0700

    Create 233-type_traits.cpp

---
+++ 233-type_traits.cpp
@@ -0,0 +1,18 @@
+#include <type_traits>
+#include <iostream>
+
+struct X {
+	int f() const && {
+		return 0;
+	}
+};
+
+int main() {
+	auto ptr = &X::f;
+	std::cout << std::is_same_v<decltype(ptr), int()> << std::is_same_v<decltype(ptr), int(X::*)()> << std::endl;
+
+	system("pause");
+	return 0;
+}
+
+// 00
