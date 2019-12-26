eb79c899b0ec157507685f4a6f642017d6fee010
Sergey Piskunov <iloveooz@yandex.ru>
2019-12-26 11:01:29 +0700

    Create 192-cout-bool.cpp

---
+++ 192-cout-bool.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> v;
+
+int f1() {
+	v.push_back(1);
+	return 0;
+}
+
+int f2() {
+	v.push_back(2);
+	return 0;
+}
+
+void g(int, int) {}
+
+void h() {
+	g(f1(), f2());
+}
+
+int main() {
+	h();
+	h();
+
+	std::cout << (v[0] == v[1]);
+
+	system("pause");
+	return 0;
+}
