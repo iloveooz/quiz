9b3f0ae2ff34540f5098f7ce3fe14bd954b61687
Sergey Piskunov <iloveooz@yandex.ru>
2019-12-30 10:40:21 +0700

    Update 109-function-lambda.cpp

--- 109-function-lambda.cpp
+++ 109-function-lambda.cpp
@@ -14,3 +14,22 @@ int main() {
 }
 
 // compilation error
+
+/*
+#include <functional>
+#include <iostream>
+
+template <typename Type>
+void call_with(std::function<void(Type)> f, Type val) {
+    f(val);
+}
+
+int main() {
+    auto print = [] (int x) { std::cout << x; };
+    call_with<int>(print, 42);
+    system("pause");
+    return 0;
+}
+*/
+
+// no compilation error
