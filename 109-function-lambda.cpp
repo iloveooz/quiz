9629ec682c0322924b935de94730edb796f48618
Sergey Piskunov <iloveooz@yandex.ru>
2019-12-30 10:38:41 +0700

    Create 109-function-lambda.cpp

---
+++ 109-function-lambda.cpp
@@ -0,0 +1,16 @@
+#include <functional>
+#include <iostream>
+
+template <typename Type>
+void call_with(std::function<void(Type)> f, Type val) {
+    f(val);
+}
+
+int main() {
+    auto print = [] (int x) { std::cout << x; };
+    call_with(print, 42);
+    system("pause");
+    return 0;
+}
+
+// compilation error
