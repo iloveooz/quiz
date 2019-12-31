d05a61878767b266d629b145d377b3d84e686e88
Sergey Piskunov <iloveooz@yandex.ru>
2019-12-31 11:01:22 +0700

    Create 187-copy-ctor.cpp

---
+++ 187-copy-ctor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+struct C {
+    C() { std::cout << "1"; }
+    C(const C& other) { std::cout << "2"; }
+    C &operator = (const C& other) {
+        std::cout << "3";
+        return *this;
+    }
+};
+
+int main() {
+    C c1;
+    C c2 = c1;
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
+
+// 12
