18cb92eea333c89f7d18514030956c61982378dd
Sergey Piskunov <iloveooz@yandex.ru>
2020-01-13 19:13:54 +0700

    Create 191-extern-c.cpp

---
+++ 191-extern-c.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+namespace  A {
+	extern "C" int x;
+}
+
+namespace  B {
+	extern "C" int x;
+}
+
+int A::x = 0;
+
+int main() {
+
+	std::cout << B::x;
+	A::x = 1;
+	std::cout << B::x;
+
+	std::cout << std::endl;
+    system("pause");
+    return 0;
+}
+
+// 01
