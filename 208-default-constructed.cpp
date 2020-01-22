37716b7ab147bc30c33472a0d3c60e3e8bcdb527
Sergey Piskunov <iloveooz@yandex.ru>
2020-01-22 14:11:39 +0700

    Create 208-default-constructed.cpp

---
+++ 208-default-constructed.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+
+bool default_constructed = false;
+bool constructed = false;
+bool assigned = false;
+
+class C {
+public:
+	C() { default_constructed = true; }
+	C(int) { constructed = true; }
+	C& operator = (const C&) { assigned = true; return *this; }
+};
+
+int main() {
+	std::map <int, C> m;
+	m[7] = C(1);
+
+	std::cout << default_constructed << constructed << assigned;
+
+	std::cout << std::endl;
+	system("pause");
+	return 0;
+}
+
+// 111
